1fd63e5b1cb2a77d82c3d36ddac917a19bf04ef9
Maxim Shmidt <history1266@mail.ru>
2022-11-28 12:49:09 +0700

    особое приветствие

---
+++ traning03/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("введите имя");
+string username = Console.ReadLine();
+if (username == "Максим"){
+    Console.WriteLine("Привет, хозяин!!");
+}
+else {
+    Console.WriteLine("Привет,");
+    Console.WriteLine(username);
+}
